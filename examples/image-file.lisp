31f91d0f60c9c83a6d5eb27539d539e6d802a347
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-07-24 01:02:27 +0000

    Modified to use the wxCL and other packages.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@27 b8c11317-770e-0410-b8d1-8a1eaaf01d41

--- examples/image-file.lisp
+++ examples/image-file.lisp
@@ -1,15 +1,22 @@
 
 
 (use-package "FFI")
-(ffi:default-foreign-language :stdc-stdcall)
+
+(load "../clisp-wrappers/wxCL.lisp")
 (load "../clisp-wrappers/wxImage.lisp")
+(load "../clisp-wrappers/wxFrame.lisp")
 (load "../clisp-wrappers/constants.lisp")
 (load "../clisp-wrappers/wx_main.lisp")
+(load "../clisp-wrappers/wx_wrapper.lisp")
 
+(use-package :wxCL)
 (use-package :wximage)
+(use-package :wxFrame)
 (use-package :wx_main)
+(use-package :wx_wrapper)
 
-(defconstant +library+ "C:/cvs-synched/commit_access/wxcl/miscellaneous/wxc-msw2.4.2-0.9.4.dll")
+(ffi:default-foreign-language :stdc)
+
 (FFI:DEF-C-TYPE wxClosure NIL)
     
 (FFI:DEF-C-TYPE
@@ -18,29 +25,40 @@
 					   (_evt (ffi:c-ptr NIL)))
 			       (:return-type NIL)))
 
-
 (ffi:def-call-out wxClosure_Create (:name "wxClosure_Create")
   (:arguments (fun ClosureFun)
 	      (data (ffi:c-pointer NIL)))
   (:return-type (ffi:c-pointer wxClosure))
-  (:library +library+))
-
-  
+  (:library +library-name+))
+ 
 
 (defun test-closure (fun data evt)
-  (let (z y f)
+  (let (z y f  a b
+	  (arr (make-array 100 :element-type 'character)))
     (print "hello world")
     (print (setf z (wximage_createfromfile "C:/Documents and Settings/Owner/Desktop/oxygen.png")))
 ;    (print (setf f (wximage_loadfile z "C:/Documents and Settings/Owner/Desktop/oxygen.png"  wxBITMAP_TYPE_PNG)))
     (print (wximage_getheight z))
     (print (wximage_getwidth z))
-    (print (wximage_destroy z))
+    (wximage_destroy z)
+    (ELJApp_Bell)
+    (setf y (wxframe_create nil -1 "hello world" 10 10 500 500 32768))
+    (print y)
     (print "hello world")
+    (wxFrame_Maximize y)
+    (print (wxFrame_IsMaximized y))
+;    (wxFrame_SetTitle y "hello")
+    (print "title set")
+;    (multiple-value-setq (a b) (wxFrame_GetTitle y))
+;    (print a)
+;    (print b)
+    (print "maximized")
+;    (ELJApp_ExitMainLoop)
     ))
 
 
 ;;Creates the closure
-;(setf x (wxClosure_Create #'test-closure nil))
+(setf x (wxClosure_Create #'test-closure nil))
 
 ;;Starts execution
 ;(Eljapp_initializeC x 0 nil)
