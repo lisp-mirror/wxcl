76ee5f34f5429755dba3bc010fe03d057fe81e1a
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-07-24 00:39:10 +0000

    Initial revision. The main package where all the parameters should be defined, also all the constants are part of this package.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@22 b8c11317-770e-0410-b8d1-8a1eaaf01d41

---
+++ clisp-wrappers/wxCL.lisp
@@ -0,0 +1,18 @@
+;;;wxCL.lisp
+;;;License   : BSD-style
+;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Surendra Singhi 
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+
+(defpackage :wxCL
+    (:use :common-lisp :ffi)
+  (:export :+library-name+))
+
+(in-package :wxCL)
+
+(defconstant +library-name+ "../miscellaneous/wxc-msw2.4.2-0.9.4.dll")
+
+
