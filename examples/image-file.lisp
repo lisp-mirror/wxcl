4587bcb8dc6180fa2b73e20366c25d4a8144c423
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-07-30 09:29:10 +0000

    Now, creates frame and dialog box.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@35 b8c11317-770e-0410-b8d1-8a1eaaf01d41

--- examples/image-file.lisp
+++ examples/image-file.lisp
@@ -1,64 +1,61 @@
 
 
 (use-package "FFI")
-
-(load "../clisp-wrappers/wxCL.lisp")
-(load "../clisp-wrappers/wxImage.lisp")
+(load "C:/cvs-synched/commit_access/wxcl/clisp-wrappers/constants.lisp")
+(load "C:/cvs-synched/commit_access/wxcl/clisp-wrappers/wxCL.lisp")
+(load "C:/cvs-synched/commit_access/wxcl/clisp-wrappers/wx_wrapper.lisp")
+(load "C:/cvs-synched/commit_access/wxcl/clisp-wrappers/wx_main.lisp")
+(load "C:/cvs-synched/commit_access/wxcl/clisp-wrappers/wxImage.lisp")
+(load "C:/cvs-synched/commit_access/wxcl/clisp-wrappers/wxWindow.lisp")
 (load "../clisp-wrappers/wxFrame.lisp")
-(load "../clisp-wrappers/constants.lisp")
-(load "../clisp-wrappers/wx_main.lisp")
-(load "../clisp-wrappers/wx_wrapper.lisp")
+(load "../clisp-wrappers/wxFileDialog.lisp")
+(load "../clisp-wrappers/wxDialog.lisp")
 
+(use-package :wxImage)
 (use-package :wxCL)
-(use-package :wximage)
-(use-package :wxFrame)
 (use-package :wx_main)
+;(use-package :wximage)
+(use-package :wxFrame)
 (use-package :wx_wrapper)
+(use-package :wxWindow)
+(use-package :wxFileDialog)
+(use-package :wxDialog)
+(ffi:default-foreign-language :c)
 
-(ffi:default-foreign-language :stdc)
 
-(FFI:DEF-C-TYPE wxClosure NIL)
-    
-(FFI:DEF-C-TYPE
-    ClosureFun (ffi:c-function (:arguments (_fun (ffi:c-ptr wxClosure))
-					   (_data (ffi:c-ptr NIL))
-					   (_evt (ffi:c-ptr NIL)))
-			       (:return-type NIL)))
+; (defun test-closure2 (fun data evt)
+;   (print "end world"))
 
-(ffi:def-call-out wxClosure_Create (:name "wxClosure_Create")
-  (:arguments (fun ClosureFun)
-	      (data (ffi:c-pointer NIL)))
-  (:return-type (ffi:c-pointer wxClosure))
-  (:library +library-name+))
- 
+; (setf test2 (wxClosure_Create #'test-closure2 nil))
 
 (defun test-closure (fun data evt)
-  (let (z y f  a b
-	  (arr (make-array 100 :element-type 'character)))
-    (print "hello world")
-    (print (setf z (wximage_createfromfile "C:/Documents and Settings/Owner/Desktop/oxygen.png")))
-;    (print (setf f (wximage_loadfile z "C:/Documents and Settings/Owner/Desktop/oxygen.png"  wxBITMAP_TYPE_PNG)))
-    (print (wximage_getheight z))
-    (print (wximage_getwidth z))
-    (wximage_destroy z)
-    (ELJApp_Bell)
-    (setf y (wxframe_create nil -1 "hello world" 10 10 500 500 32768))
-    (print y)
-    (print "hello world")
-    (wxFrame_Maximize y)
-    (print (wxFrame_IsMaximized y))
-;    (wxFrame_SetTitle y "hello")
-;    (print "title set")
-;    (multiple-value-setq (a b) (wxFrame_GetTitle y))
-;    (print a)
-;    (print b)
-    (print "maximized")
-    (ELJApp_ExitMainLoop)
+  (let (image (row 43) y filename status)
+     (setf y (wxFrame_create nil -1 "good morning" 10 10 500 500 wxDEFAULT_FRAME_STYLE))
+     (wxFrame_Maximize y)
+     (print (wxFrame_IsMaximized y))
+     (print (wxWindow_IsShown y))
+     (print (wxWindow_Show y))
+     (with-file-dialog (dialog y :message "open file" :style wxfiledialog:wxOPEN)
+       (wxDialog_ShowModal dialog)
+       (print (wxFileDialog_GetFilename dialog)))
+
+; ;    (wxFrame_SetTitle y "bye")
+;     (print "title set")
+; ;    (multiple-value-setq (a b) (wxFrame_GetTitle y))
+;     (print 5)
+;     (print 6)
+;     (sleep 10)
+;     (print (wxWindow_Destroy y))
+;     (print "maximized")
+ ;   (force-output )
+;    (ELJApp_ExitMainLoop)
     ))
 
 
 ;;Creates the closure
 (setf x (wxClosure_Create #'test-closure nil))
+
+
 
 ;;Starts execution
 ;(Eljapp_initializeC x 0 nil)
