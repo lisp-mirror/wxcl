50af84149623cced769fc784791b481d8f598381
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-07-31 11:29:56 +0000

    Removed event handling functions from this file.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@39 b8c11317-770e-0410-b8d1-8a1eaaf01d41

--- clisp-wrappers/wx_wrapper.lisp
+++ clisp-wrappers/wx_wrapper.lisp
@@ -56,19 +56,6 @@
 	   :ELJApp_IsTerminating
 	   :QuantizePalette
 	   :Quantize
-	   :wxEvtHandler_Create
-	   :wxEvtHandler_Delete
-	   :wxEvtHandler_Connect
-	   :wxEvtHandler_Disconnect
-	   :wxEvtHandler_GetNextHandler
-	   :wxEvtHandler_GetPreviousHandler
-	   :wxEvtHandler_SetNextHandler
-	   :wxEvtHandler_SetPreviousHandler
-	   :wxEvtHandler_SetEvtHandlerEnabled
-	   :wxEvtHandler_GetEvtHandlerEnabled
-	   :wxEvtHandler_ProcessEvent
-	   :wxEvtHandler_AddPendingEvent
-	   :wxEvtHandler_ProcessPendingEvents
 	   :Null_AcceleratorTable
 	   :Null_Bitmap
 	   :Null_Icon
@@ -374,95 +361,6 @@
   (:return-type ffi:int)
   (:library +library-name+))
 
-(ffi:def-call-out wxEvtHandler_Create
-    (:name "wxEvtHandler_Create")
-  (:return-type (ffi:c-pointer NIL))
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_Delete
-    (:name "wxEvtHandler_Delete")
-  (:arguments (_obj (ffi:c-pointer NIL)))
-  (:return-type NIL)
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_Connect
-    (:name "wxEvtHandler_Connect")
-  (:arguments (_obj (ffi:c-pointer NIL))
-	      (first ffi:int)
-	      (last ffi:int)
-	      (type ffi:int)
-	      (data (ffi:c-pointer NIL)))
-  (:return-type ffi:int)
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_Disconnect
-    (:name "wxEvtHandler_Disconnect")
-  (:arguments (_obj (ffi:c-pointer NIL))
-	      (first ffi:int)
-	      (last ffi:int)
-	      (type ffi:int)
-	      (data ffi:int))
-  (:return-type ffi:int)
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_GetNextHandler
-    (:name "wxEvtHandler_GetNextHandler")
-  (:arguments (_obj (ffi:c-pointer NIL)))
-  (:return-type (ffi:c-pointer NIL))
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_GetPreviousHandler
-    (:name "wxEvtHandler_GetPreviousHandler")
-  (:arguments (_obj (ffi:c-pointer NIL)))
-  (:return-type (ffi:c-pointer NIL))
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_SetNextHandler
-    (:name "wxEvtHandler_SetNextHandler")
-  (:arguments (_obj (ffi:c-pointer NIL))
-	      (handler (ffi:c-pointer NIL)))
-  (:return-type NIL)
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_SetPreviousHandler
-    (:name "wxEvtHandler_SetPreviousHandler")
-  (:arguments (_obj (ffi:c-pointer NIL))
-	      (handler (ffi:c-pointer NIL)))
-  (:return-type NIL)
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_SetEvtHandlerEnabled
-    (:name "wxEvtHandler_SetEvtHandlerEnabled")
-  (:arguments (_obj (ffi:c-pointer NIL))
-	      (enabled ffi:int))
-  (:return-type NIL)
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_GetEvtHandlerEnabled
-    (:name "wxEvtHandler_GetEvtHandlerEnabled")
-  (:arguments (_obj (ffi:c-pointer NIL)))
-  (:return-type ffi:int)
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_ProcessEvent
-    (:name "wxEvtHandler_ProcessEvent")
-  (:arguments (_obj (ffi:c-pointer NIL))
-	      (event (ffi:c-pointer NIL)))
-  (:return-type ffi:int)
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_AddPendingEvent
-    (:name "wxEvtHandler_AddPendingEvent")
-  (:arguments (_obj (ffi:c-pointer NIL))
-	      (event (ffi:c-pointer NIL)))
-  (:return-type NIL)
-  (:library +library-name+))
-
-(ffi:def-call-out wxEvtHandler_ProcessPendingEvents
-    (:name "wxEvtHandler_ProcessPendingEvents")
-  (:arguments (_obj (ffi:c-pointer NIL)))
-  (:return-type NIL)
-  (:library +library-name+))
 
 (ffi:def-call-out Null_AcceleratorTable
     (:name "Null_AcceleratorTable")
