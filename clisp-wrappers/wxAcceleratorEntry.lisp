fb053e4dc792d944270f2042392aec4c579c3db1
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-08-02 07:25:42 +0000

    Initial revision.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@45 b8c11317-770e-0410-b8d1-8a1eaaf01d41

---
+++ clisp-wrappers/wxAcceleratorEntry.lisp
@@ -0,0 +1,87 @@
+;;;wxAcceleratorEntry.lisp
+;;;License   : BSD-style
+;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Surendra Singhi 
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+
+(defpackage :wxAcceleratorEntry
+    (:use :common-lisp :ffi :wxCL :wxWindow)
+  (:export :wxAcceleratorEntry_Create
+	   :wxAcceleratorEntry_Delete
+	   :wxAcceleratorEntry_Set
+	   :wxAcceleratorEntry_GetFlags
+	   :wxAcceleratorEntry_GetKeyCode
+	   :wxAcceleratorEntry_GetCommand
+	   :wxAcceleratorTable_Create
+	   :wxAcceleratorTable_Delete
+	   :create-accelerator-entries))
+
+(in-package :wxAcceleratorEntry)
+
+(ffi:default-foreign-language :stdc)
+
+(ffi:def-call-out wxAcceleratorEntry_Create
+    (:name "wxAcceleratorEntry_Create")
+  (:arguments (flags ffi:int)
+	      (keyCode ffi:int)
+	      (cmd ffi:int))
+  (:return-type (ffi:c-pointer NIL))
+  (:library +library-name+))
+
+(ffi:def-call-out wxAcceleratorEntry_Delete
+    (:name "wxAcceleratorEntry_Delete")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxAcceleratorEntry_Set
+    (:name "wxAcceleratorEntry_Set")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (flags ffi:int)
+	      (keyCode ffi:int)
+	      (cmd ffi:int))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxAcceleratorEntry_GetFlags
+    (:name "wxAcceleratorEntry_GetFlags")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxAcceleratorEntry_GetKeyCode
+    (:name "wxAcceleratorEntry_GetKeyCode")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxAcceleratorEntry_GetCommand
+    (:name "wxAcceleratorEntry_GetCommand")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxAcceleratorTable_Create
+    (:name "wxAcceleratorTable_Create")
+  (:arguments (n ffi:int)
+	      (entries (c-array-ptr (ffi:c-pointer NIL))))
+  (:return-type (ffi:c-pointer NIL))
+  (:library +library-name+))
+
+(ffi:def-call-out wxAcceleratorTable_Delete
+    (:name "wxAcceleratorTable_Delete")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(defmacro create-accelerator-entries (frame &body body)
+  (let ((table (gensym))
+	(size (length body)))
+    `(wxWindow_SetAcceleratorTable ,frame
+      (wxAcceleratorTable_Create ,size(make-array ,size :initial-contents
+				       (list ,@(mapcar (lambda (x) `(wxAcceleratorEntry_Create ,@x)) body)))))))
+						   
+				 
---
+++ clisp-wrappers/wxPanel.lisp
@@ -0,0 +1,50 @@
+;;;wxPanel.lisp
+;;;License   : BSD-style
+;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Surendra Singhi 
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+
+(defpackage :wxPanel
+    (:use :common-lisp :ffi :wxCL)
+  (:export :wxPanel_Create
+	   :wxPanel_InitDialog
+	   :wxPanel_GetDefaultItem
+	   :wxPanel_SetDefaultItem))
+
+(in-package :wxPanel)
+
+(ffi:default-foreign-language :stdc)
+
+(ffi:def-call-out wxPanel_Create
+    (:name "wxPanel_Create")
+  (:arguments (_prt (ffi:c-pointer NIL))
+	      (_id ffi:int)
+	      (_lft ffi:int)
+	      (_top ffi:int)
+	      (_wdt ffi:int)
+	      (_hgt ffi:int)
+	      (_stl ffi:int))
+  (:return-type (ffi:c-pointer NIL))
+  (:library +library-name+))
+
+(ffi:def-call-out wxPanel_InitDialog
+    (:name "wxPanel_InitDialog")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxPanel_GetDefaultItem
+    (:name "wxPanel_GetDefaultItem")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type (ffi:c-pointer NIL))
+  (:library +library-name+))
+
+(ffi:def-call-out wxPanel_SetDefaultItem
+    (:name "wxPanel_SetDefaultItem")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (btn (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
