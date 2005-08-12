ddab6d8a13c5e6efa330f1d24dc9a0f63411d19d
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-08-12 03:09:45 +0000

    Replaces wxSingleinst.lisp
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@89 b8c11317-770e-0410-b8d1-8a1eaaf01d41

---
+++ SWIG-clisp/wxSingleInstanceChecker.lisp
@@ -0,0 +1,46 @@
+;;;wxSingleInstanceChecker.lisp
+;;;License   : BSD-style
+;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Surendra Singhi 
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+;; This is an automatically generated file. 
+;;Make changes as you feel are necessary (but remember if you try to regenerate this file, your changes will be lost). 
+
+(defpackage :wxSingleInstanceChecker
+  (:use :common-lisp :ffi)
+  (:export
+	:wxSingleInstanceChecker_CreateDefault
+	:wxSingleInstanceChecker_Create
+	:wxSingleInstanceChecker_IsAnotherRunning
+	:wxSingleInstanceChecker_Delete))
+
+(in-package :wxSingleInstanceChecker)
+
+(default-foreign-language :stdc)
+
+(def-call-out wxSingleInstanceChecker_CreateDefault
+	(:name "wxSingleInstanceChecker_CreateDefault")
+	(:return-type (c-pointer NIL))
+	(:library +library-name+))
+
+(def-call-out wxSingleInstanceChecker_Create
+	(:name "wxSingleInstanceChecker_Create")
+	(:arguments (_obj (c-pointer NIL))
+		(name (c-pointer NIL))
+		(path (c-pointer NIL)))
+	(:return-type ffi:int)
+	(:library +library-name+))
+
+(def-call-out wxSingleInstanceChecker_IsAnotherRunning
+	(:name "wxSingleInstanceChecker_IsAnotherRunning")
+	(:arguments (_obj (c-pointer NIL)))
+	(:return-type ffi:int)
+	(:library +library-name+))
+
+(def-call-out wxSingleInstanceChecker_Delete
+	(:name "wxSingleInstanceChecker_Delete")
+	(:arguments (_obj (c-pointer NIL)))
+	(:library +library-name+))
