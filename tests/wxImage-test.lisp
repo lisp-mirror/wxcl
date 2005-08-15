69c225c06ded1a6d5876fe42d3ba99d7d18cc4cd
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-08-15 06:09:38 +0000

    Changed the license to GPL and changed absolute paths to relative one.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@123 b8c11317-770e-0410-b8d1-8a1eaaf01d41

--- tests/wxImage-test.lisp
+++ tests/wxImage-test.lisp
@@ -1,9 +1,17 @@
+;;;wxImage-test.lisp
+;;;License  : GNU General Public License (GPL)
+;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Surendra Singhi 
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
 
-(load "C:/cvs-synched/commit_access/wxcl/clisp-wrappers/wxCL.lisp")
-(load "C:/cvs-synched/commit_access/wxcl/clisp-wrappers/wx_wrapper.lisp")
-(load "C:/cvs-synched/commit_access/wxcl/clisp-wrappers/wx_main.lisp")
-(load "C:/cvs-synched/commit_access/wxcl/clisp-wrappers/wxImage.lisp")
-(load "C:/clisp-2.33.83/library/lisp-unit")
+(load "../clisp-wrappers/wxCL.lisp")
+(load "../clisp-wrappers/wx_wrapper.lisp")
+(load "../clisp-wrappers/wx_main.lisp")
+(load "../clisp-wrappers/wxImage.lisp")
+(load "lisp-unit")
 
 (defpackage wxImage-test
     (:use :common-lisp :lisp-unit :wx_main :wxImage))
@@ -11,7 +19,7 @@
 (in-package :wxImage-test)
 
 (define-test png-test
-    (let ((filename "C:/cvs-synched/commit_access/wxcl/tests/oxygen.png") (row 43))
+    (let ((filename "oxygen.png") (row 43))
       (assert-true (probe-file filename) "File oxygen.png does not exists")
       (when (probe-file filename)
 	(with-wxcl
@@ -31,46 +39,31 @@
 
 ;;;the value of pixels change in gif and jpg, so cannot do the string matching test
 (define-test gif-test
-    (let ((filename "C:/cvs-synched/commit_access/wxcl/tests/oxygen.gif")(row 43))
+    (let ((filename "oxygen.gif")(row 43))
       (assert-true (probe-file filename) "File oxygen.gif does not exists")
       (when (probe-file filename)
 	(with-wxcl
 	    (unwind-protect
 		 (progn   
 		   (wxImage:with-image-file (image filename)
-		     (let ((str "smart guy, you made it. the next level is [105, 110, 116, 101, 103, 114, 105, 116, 121]"))
-		       (assert-equal 95 (wxImage:wxImage_GetHeight image) "Image height doesn't match.")
-		       (assert-equal 629 (wxImage:wxImage_GetWidth image) "Image width doesn't match.")
-;  		       (assert-prints str (loop for i from 0 to 605 by 7 do
-;  						(princ (wxImage:wxImage_GetBlue image i row))) "Image get blue failed.")
-; 		       (assert-prints str (loop for i from 0 to 605 by 7 do
-; 						(princ (wxImage:wxImage_GetRed image i row))) "Image get red failed.")
-;  		       (assert-prints str (loop for i from 0 to 605 by 7 do
-;  						(princ (wxImage:wxImage_GetGreen image i row)))
-; 		       "Image get green failed.")
-		     ))))))))
+		     (assert-equal 95 (wxImage:wxImage_GetHeight image) "Image height doesn't match.")
+		     (assert-equal 629 (wxImage:wxImage_GetWidth image) "Image width doesn't match.")
+		     )))))))
 
 (define-test jpg-test
-    (let ((filename "C:/cvs-synched/commit_access/wxcl/tests/oxygen.jpg")(row 43))
+    (let ((filename "oxygen.jpg")(row 43))
       (assert-true (probe-file filename) "File oxygen.jpg does not exists")
       (when (probe-file filename)
 	(with-wxcl
 	    (unwind-protect
 		 (progn   
 		   (with-image-file (image filename)
-		     (let ((str "smart guy, you made it. the next level is [105, 110, 116, 101, 103, 114, 105, 116, 121]"))
-		       (assert-equal 95 (wxImage:wxImage_GetHeight image) "Image height doesn't match.")
-		       (assert-equal 629 (wxImage:wxImage_GetWidth image) "Image width doesn't match.")
-; 		       (assert-prints str (loop for i from 0 to 605 by 7 do
-; 						(princ (wxImage:wxImage_GetBlue image i row))) "Image get blue failed.")
-; 		       (assert-prints str (loop for i from 0 to 605 by 7 do
-; 						(princ (wxImage:wxImage_GetRed image i row))) "Image get red failed.")
-; 		       (assert-prints str (loop for i from 0 to 605 by 7 do
-; 						(princ (wxImage:wxImage_GetGreen image i row))) "Image get green failed.")
-		       ))))))))
+		     (assert-equal 95 (wxImage:wxImage_GetHeight image) "Image height doesn't match.")
+		     (assert-equal 629 (wxImage:wxImage_GetWidth image) "Image width doesn't match.")
+		     )))))))
 
 (define-test tif-test
-    (let ((filename "C:/cvs-synched/commit_access/wxcl/tests/oxygen.tif")(row 43))
+    (let ((filename "oxygen.tif")(row 43))
       (assert-true (probe-file filename) "File oxygen.tif does not exists")
       (when (probe-file filename)
 	(with-wxcl
@@ -90,7 +83,7 @@
 
 
 
-;(lisp-unit:run-tests)
-(lisp-unit:run-tests gif-test)
+(lisp-unit:run-tests)
+;(lisp-unit:run-tests gif-test)
 
 ;(macroexpand '(lisp-unit:run-tests png-test))
