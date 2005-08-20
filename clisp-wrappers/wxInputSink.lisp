a341d717829263b053cf56d4c4621ec8c79245b4
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-08-20 05:57:14 +0000

    Initial revision.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@149 b8c11317-770e-0410-b8d1-8a1eaaf01d41

---
+++ clisp-wrappers/wxInputSink.lisp
@@ -0,0 +1,62 @@
+;;;wxInputSink.lisp
+;;;License  : GNU General Public License (GPL)
+;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Surendra Singhi 
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+
+(defpackage :wxInputSink
+    (:use :common-lisp :ffi :wxCL :wxWindow :wxDialog)
+  (:export :wxInputSink_Create
+	   :wxInputSink_GetId
+	   :wxInputSink_Start
+	   :wxInputSinkEvent_LastError
+	   :wxInputSinkEvent_LastRead
+	   :wxInputSinkEvent_LastInput))
+
+
+(in-package :wxInputSink)
+
+(ffi:default-foreign-language :stdc)
+
+(ffi:def-c-type wxInputSink NIL)
+(ffi:def-c-type wxInputSinkEvent NIL)
+    
+(ffi:def-call-out wxInputSink_Create
+    (:name "wxInputSink_Create")
+  (:arguments (input (ffi:c-pointer NIL))
+	      (evtHandler (ffi:c-pointer NIL))
+	      (bufferLen ffi:int))
+  (:return-type (ffi:c-pointer wxInputSink))
+  (:library +library-name+))
+
+(ffi:def-call-out wxInputSink_GetId
+    (:name "wxInputSink_GetId")
+  (:arguments (obj (ffi:c-pointer wxInputSink)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxInputSink_Start
+    (:name "wxInputSink_Start")
+  (:arguments (obj (ffi:c-pointer wxInputSink)))
+  (:library +library-name+))
+
+(ffi:def-call-out wxInputSinkEvent_LastError
+    (:name "wxInputSinkEvent_LastError")
+  (:arguments (obj (ffi:c-pointer wxInputSinkEvent)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxInputSinkEvent_LastRead
+    (:name "wxInputSinkEvent_LastRead")
+  (:arguments (obj (ffi:c-pointer wxInputSinkEvent)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxInputSinkEvent_LastInput
+    (:name "wxInputSinkEvent_LastInput")
+  (:arguments (obj (ffi:c-pointer wxInputSinkEvent)))
+  (:return-type ffi:c-string)
+  (:library +library-name+))
---
+++ clisp-wrappers/wxProgressDialog.lisp
@@ -0,0 +1,52 @@
+;;;wxProgressDialog.lisp
+;;;License  : GNU General Public License (GPL)
+;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Surendra Singhi 
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+
+(defpackage :wxProgressDialog
+    (:use :common-lisp :ffi :wxCL :wxWindow :wxDialog)
+    (:export :wxProgressDialog_Create
+	:wxProgressDialog_Update
+	:wxProgressDialog_UpdateWithMessage
+	:wxProgressDialog_Resume))
+
+(in-package :wxProgressDialog)
+
+(ffi:default-foreign-language :stdc)
+
+(ffi:def-c-type wxProgressDialog NIL)
+
+(ffi:def-call-out wxProgressDialog_Create
+	(:name "wxProgressDialog_Create")
+	(:arguments (title ffi:c-string)
+		(message ffi:c-string)
+		(max ffi:int)
+		(parent (ffi:c-pointer NIL))
+		(style ffi:int))
+	(:return-type (ffi:c-pointer wxProgressDialog))
+	(:library +library-name+))
+
+(ffi:def-call-out wxProgressDialog_Update
+	(:name "wxProgressDialog_Update")
+	(:arguments (obj (ffi:c-pointer wxProgressDialog))
+		(value ffi:int))
+	(:return-type ffi:int)
+	(:library +library-name+))
+
+(ffi:def-call-out wxProgressDialog_UpdateWithMessage
+	(:name "wxProgressDialog_UpdateWithMessage")
+	(:arguments (obj (ffi:c-pointer wxProgressDialog))
+		(value ffi:int)
+		(message ffi:c-string))
+	(:return-type ffi:int)
+	(:library +library-name+))
+
+(ffi:def-call-out wxProgressDialog_Resume
+	(:name "wxProgressDialog_Resume")
+	(:arguments (obj (ffi:c-pointer wxProgressDialog)))
+	(:library +library-name+))
+
