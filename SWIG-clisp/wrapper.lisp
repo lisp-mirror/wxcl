ee18fa800689193c78b49e5149ef7720c723b7b0
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-08-21 18:29:31 +0000

    License changed to wxWindows 3.1
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@154 b8c11317-770e-0410-b8d1-8a1eaaf01d41

--- SWIG-clisp/wrapper.lisp
+++ SWIG-clisp/wrapper.lisp
@@ -1,5 +1,5 @@
 ;;;wrapper.lisp
-;;;License  : GNU General Public License (GPL)
+;;;License  : wxWindows License 3.1
 ;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
 ;;;Copyright (C) 2005 Surendra Singhi 
 ;;;See the file LICENSE for information on usage and redistribution.
@@ -7,7 +7,7 @@
 ;;; $Header$
 ;;;
 ;;;wrapper.lisp
-;;;License  : GNU General Public License (GPL)
+;;;License  : wxWindows License 3.1
 ;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
 ;;;Copyright (C) 2005 Surendra Singhi 
 ;;;See the file LICENSE for information on usage and redistribution.
@@ -15,7 +15,7 @@
 ;;; $Header$
 ;;;
 ;;;wrapper.lisp
-;;;License  : GNU General Public License (GPL)
+;;;License  : wxWindows License 3.1
 ;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
 ;;;Copyright (C) 2005 Surendra Singhi 
 ;;;See the file LICENSE for information on usage and redistribution.
--- clisp-wrappers/wxValidator.lisp
+++ clisp-wrappers/wxValidator.lisp
@@ -1,5 +1,5 @@
 ;;;wxValidator.lisp
-;;;License  : GNU General Public License (GPL)
+;;;License  : wxWindows License 3.1
 ;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
 ;;;Copyright (C) 2005 Surendra Singhi 
 ;;;See the file LICENSE for information on usage and redistribution.
