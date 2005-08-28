abf17d93a0a45e88982274413ef745fe1330db32
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-08-28 02:14:31 +0000

    Changed code indentation.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@185 b8c11317-770e-0410-b8d1-8a1eaaf01d41

--- clisp-wrappers/wxRadioButton.lisp
+++ clisp-wrappers/wxRadioButton.lisp
@@ -10,11 +10,11 @@
 ;;Make changes as you feel are necessary (but remember if you try to regenerate this file, your changes will be lost). 
 
 (defpackage :wxRadioButton
-  (:use :common-lisp :ffi :wxCL)
+    (:use :common-lisp :ffi :wxCL)
   (:export :wxRadioButton_Create
-	:wxRadioButton_SetValue
-	:wxRadioButton_GetValue
-	:wxRB_GROUP))
+	   :wxRadioButton_SetValue
+	   :wxRadioButton_GetValue
+	   :wxRB_GROUP))
 
 (in-package :wxRadiobutton)
 
@@ -23,27 +23,27 @@
 (defconstant wxRB_GROUP 4)
 
 (ffi:def-call-out wxRadioButton_Create
-	(:name "wxRadioButton_Create")
-	(:arguments (_prt (ffi:c-pointer NIL))
-		(_id ffi:int)
-		(_txt ffi:c-string)
-		(_lft ffi:int)
-		(_top ffi:int)
-		(_wdt ffi:int)
-		(_hgt ffi:int)
-		(_stl ffi:int))
-	(:return-type (ffi:c-pointer NIL))
-	(:library +library-name+))
+    (:name "wxRadioButton_Create")
+  (:arguments (_prt (ffi:c-pointer NIL))
+	      (_id ffi:int)
+	      (_txt ffi:c-string)
+	      (_lft ffi:int)
+	      (_top ffi:int)
+	      (_wdt ffi:int)
+	      (_hgt ffi:int)
+	      (_stl ffi:int))
+  (:return-type (ffi:c-pointer NIL))
+  (:library +library-name+))
 
 (ffi:def-call-out wxRadioButton_SetValue
-	(:name "wxRadioButton_SetValue")
-	(:arguments (_obj (ffi:c-pointer NIL))
-		(value ffi:int))
-	(:return-type NIL)
-	(:library +library-name+))
+    (:name "wxRadioButton_SetValue")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (value ffi:int))
+  (:return-type NIL)
+  (:library +library-name+))
 
 (ffi:def-call-out wxRadioButton_GetValue
-	(:name "wxRadioButton_GetValue")
-	(:arguments (_obj (ffi:c-pointer NIL)))
-	(:return-type ffi:int)
-	(:library +library-name+))
+    (:name "wxRadioButton_GetValue")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
