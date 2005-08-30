10efb4ad4793c5c0d133d2b56e353098a8f9c570
joerg_pr <joerg_pr@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-08-30 19:39:29 +0000

    1st check-in
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@189 b8c11317-770e-0410-b8d1-8a1eaaf01d41

---
+++ sharpquote-wx/website/nodes/multi/hat.lisp
@@ -0,0 +1,36 @@
+;;; -*- Mode: LISP; Base: 10; Syntax: Common-Lisp -*-
+
+
+
+
+(with-syntax
+ (:html)
+
+ ((:div class "no-print")
+  ((:p class "hat" :css* (:lh *lh*))
+   
+
+   (:b ((:a :href* :hp-wxcl) "wxCL") " @ " 
+       ((:a :href* :sf-hp) "SourceForge")) 
+   
+   ": "
+   ((:a :href* :sf-wxcl) "Project Page") " | "
+   ((:a :href* :sf-files) "Files") " | "
+   ((:a :href* :sf-forums) "Forums") " | "
+   ((:a :href* :sf-cvs) "CVS-Tree") " | "
+   ((:a :href* :sf-bugs) "Bug Tracker") " | "
+   ((:a :href* :sf-ml-all) "Mailing Lists") ": "
+   ((:a :href* :sf-ml-devel) "wxCL-devel") ", " 
+   ((:a :href* :sf-ml-cvs) "wxCL-cvs") ", " 
+   ((:a :href* :sf-ml-all) "more")
+   
+   
+   ))
+ 
+ )
+
+
+
+
+
+ 
