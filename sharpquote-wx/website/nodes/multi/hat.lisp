6485438410df0369e56829c42b76a394c1fc495c
joerg_pr <joerg_pr@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-08-31 20:59:28 +0000

    Added dictionary lookup and macro one/language
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@196 b8c11317-770e-0410-b8d1-8a1eaaf01d41

--- sharpquote-wx/website/nodes/multi/hat.lisp
+++ sharpquote-wx/website/nodes/multi/hat.lisp
@@ -1,33 +1,32 @@
 ;;; -*- Mode: LISP; Base: 10; Syntax: Common-Lisp -*-
 
 
-
-
-(with-syntax
- (:html)
-
- ((:div class "no-print")
-  ((:p class "hat" :css* (:lh *lh*))
-   
-
-   (:b ((:a :href* :hp-wxcl) "wxCL") " @ " 
-       ((:a :href* :sf-hp) "SourceForge")) 
-   
-   ": "
-   ((:a :href* :sf-wxcl) "Project Page") " | "
-   ((:a :href* :sf-files) "Files") " | "
-   ((:a :href* :sf-forums) "Forums") " | "
-   ((:a :href* :sf-cvs) "CVS-Tree") " | "
-   ((:a :href* :sf-bugs) "Bug Tracker") " | "
-   ((:a :href* :sf-ml-all) "Mailing Lists") ": "
-   ((:a :href* :sf-ml-devel) "wxCL-devel") ", " 
-   ((:a :href* :sf-ml-cvs) "wxCL-cvs") ", " 
-   ((:a :href* :sf-ml-all) "more")
-   
-   
-   ))
+(once/language
  
- )
+ (with-syntax
+  (:html)
+  
+  ((:div class "no-print")
+   ((:p class "hat" :css* (:lh *lh*))
+    
+    
+    (:b ((:a :href* :hp-wxcl) "wxCL") " @ " 
+	((:a :href* :sf-hp) "SourceForge")) 
+    
+    ": "
+    ((:a :href* :sf-wxcl)     (babel :project-page  *language*)) " | "
+    ((:a :href* :sf-files)    (babel :files         *language*)) " | "
+    ((:a :href* :sf-forums)   (babel :forums        *language*)) " | "
+    ((:a :href* :sf-cvs)      (babel :cvs-tree      *language*)) " | "
+    ((:a :href* :sf-bugs)     (babel :bug-tracker   *lanugage*)) " | "
+    ((:a :href* :sf-ml-all)   (babel :mailing-lists *language*)) ": "
+    ((:a :href* :sf-ml-devel)        "wxCL-devel"              ) ", " 
+    ((:a :href* :sf-ml-cvs)          "wxCL-cvs"                ) ", " 
+    ((:a :href* :sf-ml-all)   (babel :more          *language*))
+    
+    ))))
+
+
 
 
 
