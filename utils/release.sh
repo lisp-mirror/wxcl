5bc11ace2fe6087dfc89042c3b41451f4c442f47
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-09-24 03:52:24 +0000

    Initial revision.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@242 b8c11317-770e-0410-b8d1-8a1eaaf01d41

---
+++ utils/release.sh
@@ -0,0 +1,18 @@
+rm -rf SWIG-clisp
+rm -rf out
+rm -rf wxcl-module
+rm -rf screenshots
+rm -rf utils
+rm -rf sharpquote-wx
+rm -rf CVSROOT
+rm -rf *~
+rm -rf */*~
+rm -rf CVS
+rm -rf */CVS
+rm -rf */*/CVS
+rm -rf */*/*CVS
+rm -rf .\#*
+rm Changes.Current
+rm .cvsignore
+rm */.cvsignore
+rm */*/.cvsignore
