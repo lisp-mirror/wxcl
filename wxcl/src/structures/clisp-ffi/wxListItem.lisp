67c1fa004a9b8023bce9fdf4eaa0efa0f18d2c65
junrue <junrue@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-10-13 22:57:29 +0000

    initial revision
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@330 b8c11317-770e-0410-b8d1-8a1eaaf01d41

---
+++ wxcl/src/structures/clisp-ffi/wxListItem.lisp
@@ -0,0 +1,221 @@
+;;;wxListItem.lisp
+;;;License  : wxWindows License 3.1
+;;;Author(s) : Jack Unrue (jdunrue@gmail.com), Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Jack D. Unrue, Surendra Singhi 
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+
+(in-package :wxcl-structures)
+
+(ffi:default-foreign-language :stdc)
+
+(ffi:def-call-out wxListItem_Create
+    (:name "wxListItem_Create")
+  (:return-type (ffi:c-pointer NIL))
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_Delete
+    (:name "wxListItem_Delete")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_Clear
+    (:name "wxListItem_Clear")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_ClearAttributes
+    (:name "wxListItem_ClearAttributes")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetMask
+    (:name "wxListItem_SetMask")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (mask ffi:int))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetId
+    (:name "wxListItem_SetId")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (id ffi:int))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetColumn
+    (:name "wxListItem_SetColumn")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (col ffi:int))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetState
+    (:name "wxListItem_SetState")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (state ffi:int))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetStateMask
+    (:name "wxListItem_SetStateMask")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (stateMask ffi:int))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetText
+    (:name "wxListItem_SetText")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (text (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetImage
+    (:name "wxListItem_SetImage")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (image ffi:int))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetData
+    (:name "wxListItem_SetData")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (data ffi:int))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetDataPointer
+    (:name "wxListItem_SetDataPointer")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (data (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetWidth
+    (:name "wxListItem_SetWidth")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (width ffi:int))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetAlign
+    (:name "wxListItem_SetAlign")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (align ffi:int))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetTextColour
+    (:name "wxListItem_SetTextColour")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (colText (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetBackgroundColour
+    (:name "wxListItem_SetBackgroundColour")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (colBack (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_SetFont
+    (:name "wxListItem_SetFont")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (font (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetMask
+    (:name "wxListItem_GetMask")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetId
+    (:name "wxListItem_GetId")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetColumn
+    (:name "wxListItem_GetColumn")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetState
+    (:name "wxListItem_GetState")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetText
+    (:name "wxListItem_GetText")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (_buf (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetImage
+    (:name "wxListItem_GetImage")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetData
+    (:name "wxListItem_GetData")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetWidth
+    (:name "wxListItem_GetWidth")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetAlign
+    (:name "wxListItem_GetAlign")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetAttributes
+    (:name "wxListItem_GetAttributes")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type (ffi:c-pointer NIL))
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_HasAttributes
+    (:name "wxListItem_HasAttributes")
+  (:arguments (_obj (ffi:c-pointer NIL)))
+  (:return-type ffi:int)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetTextColour
+    (:name "wxListItem_GetTextColour")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (_ref (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetBackgroundColour
+    (:name "wxListItem_GetBackgroundColour")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (_ref (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
+
+(ffi:def-call-out wxListItem_GetFont
+    (:name "wxListItem_GetFont")
+  (:arguments (_obj (ffi:c-pointer NIL))
+	      (_ref (ffi:c-pointer NIL)))
+  (:return-type NIL)
+  (:library +library-name+))
---
+++ wxcl/src/structures/list-item.lisp
@@ -0,0 +1,190 @@
+;;;list-item.lisp
+;;;License  : wxWindows License 3.1
+;;;Author(s): Jack Unrue (jdunrue@gmail.com), Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (c) 2005 Jack D. Unrue, Surendra Singhi
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+
+(in-package :wxcl-structures)
+
+(defclass list-item (object)
+    ()
+    (:documentation "Stores information about a list-ctrl item or column."))
+
+(defconstant +list-mask-state+        #x0001)
+(defconstant +list-mask-text+         #x0002)
+(defconstant +list-mask-image+        #x0004)
+(defconstant +list-mask-data+         #x0008)
+(defconstant +list-set-item+          #x0010) ;defined in listbase.h, but is it used?
+(defconstant +list-mask-width         #x0020)
+(defconstant +list-mask-format+       #x0040)
+
+(defconstant +list-state-dontcare+    #x0000)
+(defconstant +list-state-drophilited+ #x0001)
+(defconstant +list-state-focused+     #x0002)
+(defconstant +list-state-selected+    #x0004)
+(defconstant +list-state-cut+         #x0008)
+
+(defun make-list-item ()
+    "Create a list item data structure, for use with list-ctrl."
+    (make-wx-instance 'list-item
+        (wxListItem_Create)))
+
+(defmethod align ((obj list-item))
+    "Returns the alignment for this item."
+    (wxListItem_GetAlign (object-pointer obj)))
+
+(defmethod (setf align) (format (obj list-item))
+    "Sets the alignment for this item."
+    (wxListItem_SetAlign (object-pointer obj) format))
+
+(defmethod background-colour ((obj list-item))
+    "Returns the background colour for this item."
+    ;; FIXME: convert the pointer returned by wxListItem_GetBackgroundColour
+    ;; back into a color object.
+    ;;
+    (wxListItem_GetBackgroundColour (object-pointer obj)))
+
+(defmethod (setf background-colour) (colour (obj list-item))
+    "Sets the background colour for this item."
+    (wxListItem_SetBackgroundColour (object-pointer obj) (object-pointer colour)))
+
+(defmethod clear ((obj list-item))
+    "Resets the item state to the default."
+    (wxListItem_Clear (object-pointer obj)))
+
+(defmethod clear-attributes ((obj list-item))
+    "FIXME: undocumented method"
+    (wxListItem_ClearAttributes (object-pointer obj)))
+
+(defmethod column ((obj list-item))
+    "Returns the zero-based column; meaningful only in report mode."
+    (wxListItem_GetColumn (object-pointer obj)))
+
+(defmethod (setf column) (column (obj list-item))
+    "Sets the zero-based column; meaningful only in report mode."
+    (wxListItem_SetColumn (object-pointer obj) column))
+
+(defmethod data ((obj list-item))
+    "Returns the client data set for the item."
+    ;; FIXME: I think the wxWidgets interface here is incomplete
+    ;; for our purposes, since in Lisp you wouldn't try to coerce
+    ;; a 'long' to a pointer.  Yes, there is wxListItem_SetDataPointer,
+    ;; but there is currently no wxListItem_GetDataPointer ?
+    ;;
+    (wxListItem_GetData (object-pointer obj)))
+
+(defmethod (setf data) (value (obj list-item))
+    "Sets client data for the item."
+    ;; FIXME: I think the wxWidgets interface here is incomplete
+    ;; for our purposes, since in Lisp you wouldn't try to coerce
+    ;; a 'long' to a pointer.  Yes, there is wxListItem_SetDataPointer,
+    ;; but there is currently no wxListItem_GetDataPointer ?
+    ;;
+    (wxListItem_SetData (object-pointer obj) value))
+
+#|
+(defmethod data-pointer ((obj list-item))
+    "Returns the client data set for the item."
+    (wxListItem_GetDataPointer (object-pointer obj)))
+|#
+
+(defmethod (setf data-pointer) (value (obj list-item))
+    "Sets client data for the item."
+    ;; FIXME: need the corresponding wxListItem_GetDataPointer.
+    ;;
+    (wxListItem_SetDataPointer (object-pointer obj) value))
+
+(defmethod delete ((obj list-item))
+    "FIXME: undocumented method"
+    (wxListItem_Delete (object-pointer obj)))
+
+(defmethod font ((obj list-item))
+    "Returns the font for the item."
+    ;; FIXME: convert the pointer returned by wxListItem_GetFont back
+    ;; into a font object.
+    ;;
+    (wxListItem_GetFont (object-pointer obj)))
+
+(defmethod (setf font) (font (obj list-item))
+    "Sets the font for the item."
+    (wxListItem_SetFont (object-pointer obj) (object-pointer font)))
+
+(defmethod id ((obj list-item))
+    "Returns the zero-based item position."
+    (wxListItem_GetId (object-pointer obj)))
+
+(defmethod (setf id) (pos (obj list-item))
+    "Sets the zero-based item position."
+    (wxListItem_SetId (object-pointer obj) pos))
+
+(defmethod image ((obj list-item))
+    "Returns the zero-based index of the image associated with the \
+item in the image list."
+    (wxListItem_GetImage (object-pointer obj)))
+
+(defmethod (setf image) (index (obj list-item))
+    "Sets the zero-based index of the image associated with the \
+item into the image list."
+    (wxListItem_SetImage (object-pointer obj) index))
+
+(defmethod mask ((obj list-item))
+    "Returns a bit mask indicating which fields of the structure are \
+valid."
+    (wxListItem_GetMask (object-pointer obj)))
+
+(defmethod (setf mask) (mask (obj list-item))
+    "Sets the mask of valid fields."
+    (wxListItem_SetMask (object-pointer obj) mask))
+
+(defmethod state ((obj list-item))
+    "Returns a bit field representing the state of the item."
+    (wxListItem_GetState (object-pointer obj)))
+
+(defmethod (setf state) (bits (obj list-item))
+    "Sets the item state flags (note that the valid state flags are \
+influenced by the value of the state mask."
+    (wxListItem_SetState (object-pointer obj) bits))
+
+(defmethod state-mask ((obj list-item))
+    "Returns the bitmask that is used to determine which of the state \
+flags are to be set."
+    (wxListItem_GetStateMask (object-pointer obj)))
+
+(defmethod (setf state-mask) (mask (obj list-item))
+    "Sets the bitmask that is used to determine which of the state \
+flags are to be set."
+    (wxListItem_SetStateMask (object-pointer obj) mask))
+
+(defmethod text ((obj list-item))
+    "Returns the label/header text."
+    ;; FIXME: work through an example to see how these GetText sorts of
+    ;; methods are really supposed to work, then go back and fix the
+    ;; wrappers I have written.
+    ;;
+    (wxListItem_GetText (object-pointer obj)))
+
+(defmethod (setf text) (text (obj list-item))
+    "Sets the label/header text."
+    (wxListItem_SetText (object-pointer obj) text)) ;FIXME: need to create wxString?
+
+(defmethod text-colour ((obj list-item))
+    "Returns the text colour set for the item."
+    ;; FIXME: convert the pointer returned by wxListItem_GetTextColour
+    ;; back into a color object.
+    ;;
+    (wxListItem_GetTextColour (object-pointer obj)))
+
+(defmethod (setf text-colour) (colour (obj list-item))
+    "Sets the text colour for the item."
+    (wxListItem_SetTextColour (object-pointer obj) (object-pointer colour)))
+
+(defmethod width ((obj list-item))
+    "Returns the column width, meaningful only for column headers in report mode."
+    (wxListItem_GetWidth (object-pointer obj)))
+
+(defmethod (setf width) (value (obj list-item))
+    "Sets the column width, meaningful only for column headers in report mode."
+    (wxListItem_SetWidth (object-pointer obj) value))
