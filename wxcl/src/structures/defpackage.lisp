c8d63ea31f44a2a9510f838c7f4b616ce18fe254
junrue <junrue@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-10-13 22:57:50 +0000

    export list-item related symbols
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@331 b8c11317-770e-0410-b8d1-8a1eaaf01d41

--- wxcl/src/structures/defpackage.lisp
+++ wxcl/src/structures/defpackage.lisp
@@ -1,7 +1,7 @@
 ;;;defpackage.lisp
 ;;;License  : wxWindows License 3.1
-;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
-;;;Copyright (C) 2005 Surendra Singhi 
+;;;Author(s) : Surendra Singhi (surendra@asu.edu), Jack Unrue (jdunrue@gmail.com)
+;;;Copyright (C) 2005 Surendra Singhi, Jack D. Unrue
 ;;;See the file LICENSE for information on usage and redistribution.
 ;;;
 ;;; $Header$
@@ -9,7 +9,46 @@
 
 (defpackage :wxcl-structures
     (:use :common-lisp :ffi)
-  (:export
+    (:export
+
+     ;; list-item class
+     ;;
+     #:+list-item
+
+     ;; list-item constants
+     ;;
+	   #:+list-state-dontcare+
+	   #:+list-state-drophilited+
+	   #:+list-state-focused+ 
+	   #:+list-state-selected+
+	   #:+list-state-cut+ 
+	   #:+list-mask-state+
+	   #:+list-mask-text+ 
+	   #:+list-mask-image+
+	   #:+list-mask-data+
+	   #:+list-mask-width+
+	   #:+list-mask-format+
+
+     ;; list-item methods, functions, and macros
+     ;;
+     #:align
+     #:background-colour
+     #:clear
+     #:clear-attributes
+     #:column
+     #:data
+     #:data-pointer
+     #:delete
+     #:font
+     #:id
+     #:image
+     #:mask
+     #:state
+     #:state-mask
+     #:text
+     #:text-colour
+     #:width
+     #:make-list-item
 	   ))
 
 (in-package :wxcl-structures)
