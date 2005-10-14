e0b11e42fe1953473449a94e32a4c30e2e9dcebc
junrue <junrue@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-10-14 21:25:06 +0000

    initial revision
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@345 b8c11317-770e-0410-b8d1-8a1eaaf01d41

---
+++ wxcl/src/controls/constants.lisp
@@ -0,0 +1,20 @@
+;;;constants.lisp
+;;;License  : wxWindows License 3.1
+;;;Author(s) : Jack Unrue (jdunrue@gmail.com), Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Jack D. Unrue, Surendra Singhi
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+
+(in-package :wxcl-controls)
+
+(defconstant +align-not+               #x0000)
+(defconstant +align-center-horizontal+ #x0100)
+(defconstant +align-centre-horizontal+ +align-center-horizontal+)
+(defconstant +align-left+              +align-not+)
+(defconstant +align-top+               +align-not+)
+(defconstant +align-right+             #x0200)
+(defconstant +align-bottom+            #x0400)
+(defconstant +align-center-vertical+   #x0800)
+(defconstant +align-centre-vertical+   +align-center-vertical+)
