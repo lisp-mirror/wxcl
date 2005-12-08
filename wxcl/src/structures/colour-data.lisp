da6b76f82035bb2f59d87ea9585e939a93e37954
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-12-08 00:22:52 +0000

    Initial revision.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@393 b8c11317-770e-0410-b8d1-8a1eaaf01d41

---
+++ wxcl/src/structures/colour-data.lisp
@@ -0,0 +1,38 @@
+;;;colour-data.lisp
+;;;License  : wxWindows License 3.1
+;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Surendra Singhi 
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+
+(in-package :wxcl-structures)
+
+(ffi:default-foreign-language :stdc)
+
+(defun make-colour-data ()
+  (make-wx-instance 'colour-data
+		    (wxColourData_Create)))
+
+(defmethod delete-object ((obj colour-data))
+	(wxColourData_Delete (object-pointer obj))
+   (invalidate-wx-instance obj))
+
+(defmethod (setf choose-full) (flag (obj colour-data))
+  (wxColourData_SetChooseFull (object-pointer obj) (if flag 1 0)))
+
+(defmethod choose-full ((obj colour-data))
+  (= 1 (wxColourData_GetChooseFull (object-pointer obj))))
+
+(defmethod (setf colour) (col (obj colour-data))
+  (wxColourData_SetColour (object-pointer obj) (object-pointer col)))
+
+(defmethod colour ((obj colour-data))
+  (make-wx-instance 'wxcl-gdi:colour (wxColourData_GetColour (object-pointer obj))))
+
+(defmethod (setf custom-colour) (col (obj colour-data) i)
+	(wxColourData_SetCustomColour (object-pointer obj) i (object-pointer col)))
+
+(defmethod custom-colour ((obj colour-data) i)
+  (make-wx-instance 'wxcl-gdi:colour (wxColourData_GetCustomColour (object-pointer obj) i)))
---
+++ wxcl/src/structures/font-data.lisp
@@ -0,0 +1,65 @@
+;;;font-data.lisp
+;;;License  : wxWindows License 3.1
+;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Surendra Singhi 
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+
+(in-package :wxcl-structures)
+
+(ffi:default-foreign-language :stdc)
+
+(defun make-font-data ()
+  (make-wx-instance 'font-data
+		    (wxFontData_Create)))
+
+(defmethod delete-object ((obj font-data))
+	(wxFontData_Delete (object-pointer obj))
+   (invalidate-wx-instance obj))
+
+(defmethod (setf allow-symbols) (flag (obj font-data))
+  (wxFontData_SetAllowSymbols (object-pointer obj) (if flag 1 0)))
+
+(defmethod allow-symbols ((obj font-data))
+  (= 1 (wxFontData_GetAllowSymbols (object-pointer obj))))
+
+(defmethod (setf colour) (col (obj font-data))
+  (wxFontData_SetColour (object-pointer obj) (object-pointer col)))
+
+(defmethod colour ((obj font-data))
+  (make-wx-instance 'wxcl-gdi:colour (wxFontData_GetColour (object-pointer obj))))
+
+(defmethod (setf show-help) (flag (obj font-data))
+  (wxFontData_SetShowHelp (object-pointer obj) (if flag 1 0)))
+
+(defmethod show-help ((obj font-data))
+  (= 1 (wxFontData_GetShowHelp (object-pointer obj))))
+
+(defmethod (seyf enable-effects) (enable (obj font-data))
+  (wxFontData_EnableEffects (object-pointer obj) (if enable 1 0)))
+
+(defmethod enable-effects ((obj font-data))
+  (= 1 (wxFontData_GetEnableEffects (object-pointer obj))))
+
+(defmethod (setf initial-font) (font (obj font-data))
+  (wxFontData_SetInitialFont (object-pointer obj) (object-pointer font)))
+
+(defmethod initial-font ((obj font-data))
+  (make-wx-instance 'wxcl-gdi:font (wxFontData_GetInitialFont (object-pointer obj))))
+
+(defmethod (setf chosen-font) (font (obj font-data))
+  (wxFontData_SetChosenFont (object-pointer obj) (object-pointer font)))
+
+(defmethod chosen-font ((obj font-data))
+  (make-wx-instance 'wxcl-gdi:font (wxFontData_GetChosenFont (object-pointer obj))))
+
+(defmethod set-range ((obj font-data) min max)
+  (wxFontData_SetRange (object-pointer obj) min max))
+
+(defmethod encoding ((obj font-data))
+  (wxFontData_GetEncoding (object-pointer obj)))
+
+(defmethod (setf encoding) (encoding (obj font-data))
+  (wxFontData_SetEncoding (object-pointer obj) encoding))
