7a959476d8c446d9ded7b9b7ebe482e9bee22f44
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-12-09 02:55:52 +0000

    Initial revision.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@399 b8c11317-770e-0410-b8d1-8a1eaaf01d41

---
+++ wxcl/src/layout/static-box-sizer.lisp
@@ -0,0 +1,20 @@
+;;;window.lisp
+;;;License  : wxWindows License 3.1
+;;;Author(s) : Surendra Singhi (surendra@asu.edu) 
+;;;Copyright (C) 2005 Surendra Singhi 
+;;;See the file LICENSE for information on usage and redistribution.
+;;;
+;;; $Header$
+;;;
+
+(in-package :wxcl-layout)
+
+(defun make-static-box-sizer (static-box orient)
+  "Create a static box sizer."
+    (make-wx-instance 'static-box-sizer
+                      (wxStaticBoxSizer_Create (object-pointer static-box) orient)))
+
+
+(defmethod GetStaticBox ((obj static-box-sizer))
+  (make-wx-instance 'wxcl-controls:static-box 
+                    (wxStaticBoxSizer_GetStaticBox (object-pointer obj))))
