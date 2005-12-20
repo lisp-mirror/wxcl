9966b56bf5b42643e802e8d42057b2c5d5f5f4dc
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2005-12-20 10:27:56 +0000

    Changed to account for cffi bindings.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@417 b8c11317-770e-0410-b8d1-8a1eaaf01d41

--- wxcl/examples/hello-world.lisp
+++ wxcl/examples/hello-world.lisp
@@ -1,10 +1,15 @@
 (asdf:operate 'asdf:load-op 'wxcl)
 
-(defun init-func (evt)
+; (defun init-func (evt)
+;   (let ((fr (wxcl-windows:make-frame nil -1 "hello world")))
+;     (wxcl-windows:show fr)))
+
+(cffi:defcallback init-func :void ((evt :pointer))
   (let ((fr (wxcl-windows:make-frame nil -1 "hello world")))
     (wxcl-windows:show fr)))
 
-(wxcl:start-app #'init-func)
+;(wxcl:start-app #'init-func)
+(wxcl:start-app (cffi:callback init-func))
 
 
 
