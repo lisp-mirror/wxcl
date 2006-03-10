3d9d274b719e751fae121e86784e9057c64a0f9c
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2006-03-10 11:09:32 +0000

    Initial revision. Generate cffi code using the SWIG interface file. Right now very much meant for cygwin.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@453 b8c11317-770e-0410-b8d1-8a1eaaf01d41

---
+++ wxcl/wx-swig.sh
@@ -0,0 +1,50 @@
+WXWIN="c:/wxWidgets-2.6.2"
+WXCL="c:/cvs-synched/commit-access/wxcl"
+WXDIR="$WXWIN"
+WXCLDIR="$WXCL"
+WXC="$WXCL/../wxc"
+
+WXVERSION="2.6.2"
+if test $DEBUG 
+then
+     DEBUGPOSTFIX="d"
+else
+     DEBUGPOSTFIX=""
+fi
+
+WXSRC="$WXDIR/src/msw"
+WXINC="$WXDIR/include"
+WXLIBDIR="$WXDIR/lib/vc_lib"
+INCTEMP="$WXDIR/lib/vc_lib/msw$DEBUGPOSTFIX"
+
+WXCLSWIG="$WXCL/SWIG"
+WXCLSWIGCLASSES="$WXCL/SWIG/classes"
+WXCEWXW="$WXCL/wxc/src/ewxw"
+
+SWIGLANG="cffi"
+
+wx_flags=`echo "-I$WXC " "-I$WXCEWXW " "-I$WXDIR " "-I$WXINC " "-D__WXMSW__ " "-I$INCTEMP "`  #"-c++ "
+
+swig_cmd="swig"
+
+cffi_dir="$WXCL/src/cffi"
+
+cffi_classes_dir="$cffi_dir/classes"
+
+`mkdir $cffi_classes_dir`
+
+files=`ls -B $WXCLSWIGCLASSES`
+
+for f in $files   #'wxButton.i' 
+  do
+  
+  mod_name=`echo $f | awk -F. '{print $1}'`
+  echo "swig -$SWIGLANG $wx_flags -o \"$cffi_classes_dir/$mod_name.lisp\" $WXCLSWIGCLASSES/$f" 
+  `swig -$SWIGLANG $wx_flags -o "$cffi_classes_dir/$mod_name.lisp" $WXCLSWIGCLASSES/$f`  
+done
+
+echo "swig -$SWIGLANG $wx_flags -o \"$cffi_dir/wx_main.lisp\" $WXCLSWIG/wx_main.i" 
+`swig -$SWIGLANG $wx_flags -o "$cffi_dir/wxmain.lisp" $WXCLSWIG/wx_main.i`
+
+echo "swig -$SWIGLANG $wx_flags -o \"$cffi_dir/wx_wrapper.lisp\" $WXCLSWIG/wx_wrapper.i" 
+`swig -$SWIGLANG $wx_flags -o "$cffi_dir/wxwrapper.lisp" $WXCLSWIG/wx_wrapper.i` 
