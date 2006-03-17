7f2bc5ebd2db32054d6da56203554359c5c998b6
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2006-03-17 14:56:29 +0000

    Makes use of new event system, and cffi now.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@462 b8c11317-770e-0410-b8d1-8a1eaaf01d41

--- wxcl/examples/hello-world.lisp
+++ wxcl/examples/hello-world.lisp
@@ -1,21 +1,18 @@
 (asdf:operate 'asdf:load-op 'cffi)
-(push :cffi *features*)
 ;#+cffi (cffi:load-foreign-library wxcl:+library-name+)
 
 (asdf:operate 'asdf:load-op 'wxcl)
 
+(defun init-func (evt)
+   (let ((fr (wxcl:make-frame nil -1 "hello world")) title)
+;     (setf title (wxcl:title fr))
+     (print (wxcl::wxString_GetLength title))
+     (print (wxcl::wxStringc_str title))
+     (print wxcl::wxEVENT_GRID_EDITOR_HIDDEN)
+     (print (cffi:mem-ref wxcl::wxEVENT_GRID_EDITOR_HIDDEN :int))
+     (wxcl:show fr)))
 
-; (defun init-func (evt)
-;   (let ((fr (wxcl-windows:make-frame nil -1 "hello world")))
-;     (wxcl-windows:show fr)))
-
-(cffi:defcallback init-func :void ((evt :pointer))
-  (let ((fr (wxcl-windows:make-frame nil -1 "hello world")))
-    (print (wxcl-windows:title fr))
-    (wxcl-windows:show fr)))
-
-;(wxcl:start-app #'init-func)
-(wxcl:start-app (cffi:callback init-func))
+(wxcl:start-app #'init-func)
 
 
 
