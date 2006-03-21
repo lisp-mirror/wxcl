45b81bed2c91b574be8a892d6197f8f760fb15ba
efuzzyone <efuzzyone@b8c11317-770e-0410-b8d1-8a1eaaf01d41>
2006-03-21 08:29:24 +0000

    Removed some comments.
    git-svn-id: https://svn.code.sf.net/p/wxcl/svn/trunk@476 b8c11317-770e-0410-b8d1-8a1eaaf01d41

--- wxcl/examples/hello-world.lisp
+++ wxcl/examples/hello-world.lisp
@@ -1,13 +1,9 @@
 (asdf:operate 'asdf:load-op 'cffi)
-;#+cffi (cffi:load-foreign-library wxcl:+library-name+)
-
 (asdf:operate 'asdf:load-op 'wxcl)
 
 (defun init-func (evt)
   (let ((fr (wxcl:make-frame nil -1 "hello world")) title)
     (print (wxcl:title fr))
-;    (print wxcl::wxEVENT_GRID_EDITOR_HIDDEN)
-;    (print (cffi:mem-ref wxcl::wxEVENT_GRID_EDITOR_HIDDEN :int))
     (wxcl:show fr)))
 
 (wxcl:start-app #'init-func)
